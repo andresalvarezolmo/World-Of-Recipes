13b5135c2f6d7d0dc709b9634328d6272606deff
Andres <xxdrews@gmail.com>
2021-04-13 19:21:48 +0100

    drag and drop working with colouring functionality

--- Assets/Scripts/DragSystem.cs
+++ Assets/Scripts/DragSystem.cs
@@ -6,6 +6,10 @@ public class DragSystem : MonoBehaviour
 {
     public GameObject dragArea;
     private bool isMoving;
+    private bool selected;
+    SpriteRenderer sprite;
+
+    private Color originalColor;
 
     private float startingPositionX;
     private float startingPositionY;
@@ -13,11 +17,14 @@ public class DragSystem : MonoBehaviour
     void Start()
     {
         originalPosition = this.transform.localPosition;
+        sprite = GetComponent<SpriteRenderer>();
+        originalColor = GetComponent<SpriteRenderer>().material.GetColor("_Color");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //update location of sprite
         if (isMoving)
         {
             Vector3 mousePos;
@@ -29,29 +36,36 @@ public class DragSystem : MonoBehaviour
 
     private void OnMouseDown()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButtonDown(0) && selected == false)
         {
             Vector3 mousePos;
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
             startingPositionX = mousePos.x - this.transform.localPosition.x;
             startingPositionY = mousePos.y - this.transform.localPosition.y;
-
             isMoving = true;
+        }
+        //deselect sprite
+        if (Input.GetMouseButtonDown(0) && selected == true)
+        {
+            Debug.Log("DESELECTED");
+            selected = false;
+            sprite.color = originalColor;
         }
     }
 
     private void OnMouseUp()
     {
         isMoving = false;
+        //if sprite has been dragged into the dragArea then mark it as selected
         if(Mathf.Abs(this.transform.localPosition.x - dragArea.transform.localPosition.x) <= 3 && 
            Mathf.Abs(this.transform.localPosition.y - dragArea.transform.localPosition.y) <= 3){
-            this.transform.localPosition = new Vector3(dragArea.transform.localPosition.x, dragArea.transform.localPosition.y, dragArea.transform.localPosition.z);
+            //this.transform.localPosition = new Vector3(dragArea.transform.localPosition.x, dragArea.transform.localPosition.y, dragArea.transform.localPosition.z);
+            sprite.color = new Color(0.3f, 0.4f, 0.6f);
+            Debug.Log("SELECTED"); 
+            selected = true;
         }
-        else
-        {
-            this.transform.localPosition = new Vector3(originalPosition.x, originalPosition.y, originalPosition.z);
-        }
+        this.transform.localPosition = new Vector3(originalPosition.x, originalPosition.y, originalPosition.z);
     }
 }
